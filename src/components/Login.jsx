7c01eed71deaedf2572b7a64bb4e4852932c1d41
santoshkbrj <santoshsah017@gmal.com>
2024-05-29 22:43:35 +0530

    Style tags with with module css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,9 @@
+import style from '../styles/Login.module.css';
 
 export default function Login({ login }) {
     return (
         <div>
-            {login ? <div>John is login</div> : <div>{name} John is not login</div>}
+            {login ? <div className={style.login}>John is login</div> : <div className={style.login}> John is not login</div>}
         </div>
     )
 }
