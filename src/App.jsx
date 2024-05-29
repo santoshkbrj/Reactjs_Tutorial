797c5987db307d1e329f82e5edbb509bc9eaa8a3
santoshkbrj <santoshsah017@gmal.com>
2024-05-29 23:49:51 +0530

    Input file on Chnage Event

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-
+import { useState } from "react";
 
 function App() {
-    function clickHandler() {
-        alert('hello Wolrd')
-    }
+    const [username, setUsername] = useState('');
+    const [password, setPasword] = useState('');
+    const [users, setUsers] = useState([]);
+
+
     return (
         <>
-            <button onClick={clickHandler}>Click Me</button>
+            <form>
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" value={password} onChange={(e) => setPasword(e.target.value)} />
+                </div>
+                <button onClick={(e) => {
+                    e.preventDefault();
+                    const newUser = {
+                        username,
+                        password
+                    }
+                    setUsers((currUser) => [...currUser, newUser])
+                    console.log(users)
+                }}>Add User</button>
 
+            </form>
+            <p>Username: {username}</p>
+            <p>Password: {password}</p>
         </>
     )
 }
