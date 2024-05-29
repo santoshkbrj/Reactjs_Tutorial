354ce723fa00f4d9ea8d7c8ce91a5525bd9d2571
santoshkbrj <santoshsah017@gmal.com>
2024-05-29 21:58:59 +0530

    Added userprofile detail in new component

---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,10 @@
+function UserProfile() {
+    return (
+        <div>
+            <p>Name: John Doe</p>
+            <p>Age: 35</p>
+            <p>Email: john@gmail.comm</p>
+        </div>
+    )
+}
+export default UserProfile;
