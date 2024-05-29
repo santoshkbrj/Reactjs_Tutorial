d099d742b48f96d640940eb4978e2ac38d3f5c80
santoshkbrj <santoshsah017@gmal.com>
2024-05-29 22:34:54 +0530

    Style css with inline css

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ function UserProfile({ userdata }) {
     const isLogin = true;
     return (
         <div>
-            <p>Name: {userdata.name}</p>
+            <p style={{ color: "#000", fontSize: "18px", fontWeight: "bold", backgroundColor: "green" }}>Name: {userdata.username}</p>
             <p>Age: {userdata.age}</p>
             <p>Email: {userdata.email}</p>
             <hr />
