aad0078fa0de97db3e1978c0781fd679e4c89fc2
santoshkbrj <santoshsah017@gmal.com>
2024-05-30 23:32:24 +0530

    userEffect hook and it's dependancy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useEffect } from "react";
+import { useState } from "react";
 
-import UserState from "./components/UserState";
 function App() {
+    const [counter, setCounter] = useState(0);
+    const [sync, setSync] = useState(false)
+
+    useEffect(() => {
+        console.log('rendering....');
+        document.title = "React tutorial " + counter
+    }, [sync])
 
     return (
         <>
-            <UserState />
+            <div>You click button {counter} times</div>
+            <button onClick={() => setCounter((count) => count + 1)} >Click Me</button>
+            <button onClick={() => setSync((currentState) => !currentState)} >Sync Me</button>
         </>
     )
 }
